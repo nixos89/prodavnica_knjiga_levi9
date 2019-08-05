757e8c41330b31c5d7eae9d7013b77882f1bda8e
Aleksandar Jeremic <35471961+JeremicAleks@users.noreply.github.com>
2019-08-05 17:14:36 +0200

    navbar

--- prodavnicaClient/src/app/app.module.ts
+++ prodavnicaClient/src/app/app.module.ts
@@ -7,11 +7,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 import { CoreModule } from './core/core.module';
+import { NavbarComponent } from './elements/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddBookComponent
+    AddBookComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule, AppRoutingModule, CoreModule, BrowserAnimationsModule, ToastrModule.forRoot()
