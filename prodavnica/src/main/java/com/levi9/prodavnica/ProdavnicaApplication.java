f6e45bfe5c47aa944fd1f7ac377a90e812a45c0d
Aleksandar Jeremic <35471961+JeremicAleks@users.noreply.github.com>
2019-08-05 12:53:37 +0200

    Spring Boot Initializr

---
+++ prodavnica/src/main/java/com/levi9/prodavnica/ProdavnicaApplication.java
@@ -0,0 +1,13 @@
+package com.levi9.prodavnica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdavnicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdavnicaApplication.class, args);
+	}
+
+}
---
+++ prodavnica/src/test/java/com/levi9/prodavnica/ProdavnicaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.levi9.prodavnica;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProdavnicaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
