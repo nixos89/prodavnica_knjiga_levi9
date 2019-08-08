71aa0db486b5ebc6dd26e4e2bfe645d3962ce07e
Nikola Stevanovic <nixos89@gmail.com>
2019-08-08 11:50:07 +0200

    Filled home page with books and categories

--- prodavnica/src/main/resources/db/changelogs/fillDummyData.sql
+++ prodavnica/src/main/resources/db/changelogs/fillDummyData.sql
@@ -6,16 +6,18 @@ INSERT INTO author(author_id, first_name, last_name) VALUES(4, "JK", "Rowling");
 
 -- Inserting books
 
-INSERT INTO book(book_id, amount, is_deleted, name, price) VALUES(1, 33, 0, "Harry Potter", 999.99);
-INSERT INTO book(book_id, amount, is_deleted, name, price) VALUES(2, 151, 0, "Biblbija Novi Zavet", 1099.98);
-INSERT INTO book(book_id, amount, is_deleted, name, price) VALUES(3, 5, 0, "Gorski vijenac", 39.98);
-INSERT INTO book(book_id, amount, is_deleted, name, price) VALUES(4, 7, 0, "Pazi kojeg vuka hranis", 682.50);
-INSERT INTO book(book_id, amount, is_deleted, name, price) VALUES(5, 85, 0, "Uvelici uveoci", 774.99);
+INSERT INTO book(book_id, amount, is_deleted, name, price) VALUES(1, 33, 0, "Harry Potter", 29.99);
+INSERT INTO book(book_id, amount, is_deleted, name, price) VALUES(2, 151, 0, "Biblbija Novi Zavet", 19.98);
+INSERT INTO book(book_id, amount, is_deleted, name, price) VALUES(3, 5, 0, "Gorski vijenac", 20.98);
+INSERT INTO book(book_id, amount, is_deleted, name, price) VALUES(4, 7, 0, "Pazi kojeg vuka hranis", 15.50);
+INSERT INTO book(book_id, amount, is_deleted, name, price) VALUES(5, 85, 0, "Uvelici uveoci", 14.99);
 
 -- connecting author and book
 INSERT INTO book_author(author_id, book_id) VALUES(1, 4);
 INSERT INTO book_author(author_id, book_id) VALUES(3, 5);
 INSERT INTO book_author(author_id, book_id) VALUES(4, 1);
+INSERT INTO book_author(author_id, book_id) VALUES(2, 2);
+INSERT INTO book_author(author_id, book_id) VALUES(3, 2);
 INSERT INTO book_author(author_id, book_id) VALUES(2, 3);
 
 -- inserting categories
