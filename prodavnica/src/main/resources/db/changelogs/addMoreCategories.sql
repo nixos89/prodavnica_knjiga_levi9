4bbd6dba4029e9973e3e8967886885ba38412e5a
Nikola Stevanovic <nixos89@gmail.com>
2019-08-09 17:05:28 +0200

    Added more categories to db

---
+++ prodavnica/src/main/resources/db/changelogs/addMoreCategories.sql
@@ -0,0 +1,17 @@
+-- inserting categories
+INSERT INTO category(category_id, name, is_deleted) VALUES(5, "Horor", 0);
+INSERT INTO category(category_id, name, is_deleted) VALUES(6, "Romansa", 0);
+INSERT INTO category(category_id, name, is_deleted) VALUES(7, "Drama", 0);
+INSERT INTO category(category_id, name, is_deleted) VALUES(8, "Poezija", 0);
+INSERT INTO category(category_id, name, is_deleted) VALUES(9, "Krimi", 0);
+INSERT INTO category(category_id, name, is_deleted) VALUES(10, "Akcija", 0);
+INSERT INTO category(category_id, name, is_deleted) VALUES(11, "Fantazija", 0);
+INSERT INTO category(category_id, name, is_deleted) VALUES(12, "Duh i telo", 0);
+INSERT INTO category(category_id, name, is_deleted) VALUES(13, "Filozofija", 0);
+INSERT INTO category(category_id, name, is_deleted) VALUES(14, "Istorija", 0);
+INSERT INTO category(category_id, name, is_deleted) VALUES(15, "Kuvari", 0);
+INSERT INTO category(category_id, name, is_deleted) VALUES(16, "Putopisi", 0);
+INSERT INTO category(category_id, name, is_deleted) VALUES(17, "Psihologija", 0);
+INSERT INTO category(category_id, name, is_deleted) VALUES(18, "Teorije zavere", 0);
+INSERT INTO category(category_id, name, is_deleted) VALUES(19, "Umetnost", 0);
+INSERT INTO category(category_id, name, is_deleted) VALUES(20, "Mitologije", 0);
