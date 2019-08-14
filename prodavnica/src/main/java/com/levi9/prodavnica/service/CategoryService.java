385a7e996c2a594b0460bf421028b045c6cbfec0
Nikola Stevanovic <nixos89@gmail.com>
2019-08-14 13:45:08 +0200

    added to resolve conflicts when merging with master branch

---
+++ prodavnica/src/main/java/com/levi9/prodavnica/service/CategoryService.java
@@ -0,0 +1,24 @@
+package com.levi9.prodavnica.service;
+
+import java.util.Set;
+
+import com.levi9.prodavnica.dto.AddCategoryDTO;
+import com.levi9.prodavnica.dto.BookListDTO;
+import com.levi9.prodavnica.dto.CategoryDTO;
+import com.levi9.prodavnica.dto.CategoryListDTO;
+
+public interface CategoryService {
+
+	CategoryListDTO findAllCategories();
+
+	CategoryDTO getOne(Long id);
+
+	CategoryDTO addCategory(AddCategoryDTO addCategoryDTO);
+
+	Boolean updateCategory(AddCategoryDTO addCategoryDTO, Long id);
+
+	Boolean deleteCategory(Long id);
+
+	BookListDTO getAllBooksFromCategories(Set<Long> id);
+
+}
