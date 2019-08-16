b13ccc238b9b579ba491ebf2c86476e6de2e98f7
Nikola Stevanovic <nixos89@gmail.com>
2019-08-16 17:42:48 +0200

    added prefix

--- prodavnica/src/test/java/com/levi9/prodavnica/config/UrlPrefix.java
+++ prodavnica/src/test/java/com/levi9/prodavnica/config/UrlPrefix.java
@@ -6,5 +6,5 @@ public class UrlPrefix {
 	public static final String GET_AUTHORS = "/api/authors";
 	public static final String GET_BOOKS = "/api/books";
 	public static final String GET_ORDERS = "/api/orders";
-
+	public static final String GET_TOP_SELLING_BOOKS = "/api/books/topSellingBooksLimit";
 }
