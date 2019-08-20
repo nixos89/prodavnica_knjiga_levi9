bc685b545b7e9a1f4a23d775ee36c1a0f88b56a1
Nikola Stevanovic <nixos89@gmail.com>
2019-08-20 15:33:37 +0200

    initial commit

---
+++ prodavnica/src/main/java/com/levi9/prodavnica/utils/PDFGenerator.java
@@ -0,0 +1,17 @@
+package com.levi9.prodavnica.utils;
+
+import java.io.ByteArrayInputStream;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PDFGenerator {
+	
+	private static Logger logger = LoggerFactory.getLogger(PDFGenerator.class);
+	
+//	public static ByteArrayInputStream ordersPDFReport(List<Order>) {
+//		
+//	}
+	
+}
