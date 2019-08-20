78f29765af58adc93c5c6049cbd04c5ad612cd98
Nikola Stevanovic <nixos89@gmail.com>
2019-08-20 17:49:16 +0200

    created findAll method

--- prodavnica/src/main/java/com/levi9/prodavnica/repository/OrderRepository.java
+++ prodavnica/src/main/java/com/levi9/prodavnica/repository/OrderRepository.java
@@ -1,11 +1,19 @@
 package com.levi9.prodavnica.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import com.levi9.prodavnica.dto.OrderDTO;
 import com.levi9.prodavnica.model.Order;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
-
+		
+	public List<Order> findAll();
+	
+	
+	
 }
