2c7f3a2327f3d17419c5904068033e4c67643710
Nikola Stevanovic <nixos89@gmail.com>
2019-08-21 12:37:21 +0200

    created methods for retrieving PDF report

--- prodavnica/src/main/java/com/levi9/prodavnica/controller/OrderController.java
+++ prodavnica/src/main/java/com/levi9/prodavnica/controller/OrderController.java
@@ -1,6 +1,12 @@
 package com.levi9.prodavnica.controller;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +16,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.levi9.prodavnica.dto.OrderListDTO;
+import com.levi9.prodavnica.dto.OrderReportDTO;
 import com.levi9.prodavnica.service.OrderService;
+import com.levi9.prodavnica.utils.PDFGenerator;
 
 @RestController
 @RequestMapping("api/orders")
@@ -25,8 +33,22 @@ public class OrderController {
 		return ResponseEntity.ok(orderService.addOrder(orderRequest));
 	}
 
-	@GetMapping
+	@GetMapping()
 	public ResponseEntity<?> getOrderReport(){
 		return ResponseEntity.ok(orderService.getOrderReport());
+	}
+	
+	@GetMapping("/pdf")
+	public ResponseEntity<InputStreamResource> orderReportPdf() throws IOException{
+		OrderReportDTO orderReportDTO = orderService.getOrderReport();
+		ByteArrayInputStream bis = PDFGenerator.ordersPDFReport(orderReportDTO);
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Content-Disposition", "inline; filename=orders.pdf");
+		
+		return ResponseEntity.ok()
+				.headers(headers)
+				.contentType(MediaType.APPLICATION_PDF)
+				.body(new InputStreamResource(bis));
 	}
 }
