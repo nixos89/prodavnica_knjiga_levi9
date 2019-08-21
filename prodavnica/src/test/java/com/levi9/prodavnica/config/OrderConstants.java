f2107a284fba6681c747e61a72cb7ce7e34b17ad
Nikola Stevanovic <nixos89@gmail.com>
2019-08-21 17:46:03 +0200

    created tests for processed orders for controller

--- prodavnica/src/test/java/com/levi9/prodavnica/config/OrderConstants.java
+++ prodavnica/src/test/java/com/levi9/prodavnica/config/OrderConstants.java
@@ -1,23 +1,37 @@
 package com.levi9.prodavnica.config;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
 
 import com.levi9.prodavnica.dto.AddOrderDTO;
+import com.levi9.prodavnica.dto.BookDTO;
+import com.levi9.prodavnica.dto.OrderDTO;
+import com.levi9.prodavnica.dto.OrderItemDTO;
 import com.levi9.prodavnica.dto.OrderListDTO;
+import com.levi9.prodavnica.dto.OrderReportDTO;
 import com.levi9.prodavnica.dto.OrderResponseDTO;
 import com.levi9.prodavnica.model.Book;
 import com.levi9.prodavnica.model.Order;
 
 public class OrderConstants {
 
-	public static final double order0total = 100;
+	public static final double order0total = 100; // order total price
 	public static final int order0amount = 5;
 	public static final Long order0bookId = 1L;
-	public static final Long order0orderId = 1L; 
-
-	public static final int order1amount = 20;
+	public static final Long order0orderId = 1L;
+	public static final Long order0orderItem0Id = 1L;
+	public static final Long order0orderItem1Id = 2L;
+	public static final double order0orderPrice = 50;
+	public static final String order0Date = "21/08/2019 10:15:16";
+	
+	public static final int order0orderItem0amount = 4;
+	public static final int order0orderItem1amount = 3;
+	
+	public static final double order0Price = BookConstants.book0price * order0orderItem0amount;
 
 	public static final Set<AddOrderDTO> addOrders = new HashSet<>();
 	public static final AddOrderDTO addOrderRequestExpected = new AddOrderDTO(OrderConstants.order0amount,
@@ -35,5 +49,39 @@ public class OrderConstants {
 	public static final OrderListDTO orderListDTO = new OrderListDTO(addOrders, order0total);
 
 	public static final OrderListDTO orderListNull = null;
+	
+	public static final OrderReportDTO orderReportDTO = new OrderReportDTO();
+	public static final List<OrderDTO> orderDTOList = orderItemDTOs();
+	
+	
+	
+	public static List<OrderDTO> orderItemDTOs(){
+		LinkedList<OrderItemDTO> orderItemDTOList = new LinkedList<OrderItemDTO>();
+		
+		OrderItemDTO oi0DTO1 = new OrderItemDTO();
+		oi0DTO1.setOrderId(order0orderId);
+		oi0DTO1.setOrderedAmount(order0orderItem0amount);
+		oi0DTO1.setOrderItemId(order0orderItem0Id);
+		oi0DTO1.setTotalOrderedItemPrice(order0orderItem0amount * BookConstants.book0price);
+		oi0DTO1.setBookDTO(new BookDTO(BookConstants.book0id, BookConstants.book0name, BookConstants.book0price, BookConstants.book0amount, BookConstants.book0deleted));
+		
+		OrderItemDTO oi0DTO2 = new OrderItemDTO();
+		oi0DTO2.setOrderId(order0orderId);
+		oi0DTO2.setOrderedAmount(order0orderItem1amount);
+		oi0DTO2.setOrderItemId(order0orderItem1Id);
+		oi0DTO2.setTotalOrderedItemPrice(order0orderItem1amount * BookConstants.book1price);
+		oi0DTO2.setBookDTO(new BookDTO(BookConstants.book0id, BookConstants.book0name, BookConstants.book0price, BookConstants.book0amount, BookConstants.book0deleted));
+		
+		orderItemDTOList.add(oi0DTO1);
+		orderItemDTOList.add(oi0DTO2);
+		
+		List<OrderDTO> orderDTOListNew = new ArrayList<>();
+		OrderDTO oDTO0 = new OrderDTO(order0orderId, orderItemDTOList, order0Date, order0Price);
+		orderDTOListNew.add(oDTO0);
+		
+		return orderDTOListNew;
+	}
+	
+	
 
 }
--- prodavnica/src/test/java/com/levi9/prodavnica/controller/OrderControllerTest.java
+++ prodavnica/src/test/java/com/levi9/prodavnica/controller/OrderControllerTest.java
@@ -1,13 +1,17 @@
 package com.levi9.prodavnica.controller;
 
-import static org.mockito.Mockito.when;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,12 +20,15 @@ import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDoc
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.FieldDescriptor;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.levi9.prodavnica.config.BookConstants;
 import com.levi9.prodavnica.config.OrderConstants;
 import com.levi9.prodavnica.config.UrlPrefix;
+import com.levi9.prodavnica.dto.OrderReportDTO;
 import com.levi9.prodavnica.dto.OrderResponseDTO;
 import com.levi9.prodavnica.service.OrderService;
 
@@ -48,5 +55,45 @@ public class OrderControllerTest {
 				.andExpect(jsonPath("$.orderId").value(OrderConstants.order0orderId))
 				.andDo(document("{class-name}/{method-name}"));
 	}
+	
+	public void getProcessedOrdersTest() throws Exception {
+		OrderReportDTO orDTO = new OrderReportDTO();
+		orDTO.setOrderDTOList(OrderConstants.orderDTOList);
+		when(orderService.getOrderReport()).thenReturn(orDTO);
+		
+		mockMvc.perform(get(UrlPrefix.GET_ORDERS).accept(MediaType.APPLICATION_JSON_VALUE))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.orderDTOList[0].orderId").value(OrderConstants.order0orderId))
+			.andExpect(jsonPath("$.orderDTOList[0].orderDate").value(OrderConstants.order0Date))
+			.andExpect(jsonPath("$.orderDTOList[0].orderPrice").value(OrderConstants.order0Price))
+			.andExpect(jsonPath("$.orderDTOList[0].orderItemDTOList[0].orderItemId").value(OrderConstants.order0orderItem0Id))
+			.andExpect(jsonPath("$.orderDTOList[0].orderItemDTOList[0].orderId").value(OrderConstants.order0orderId))
+			.andExpect(jsonPath("$.orderDTOList[0].orderItemDTOList[0].orderedAmount").value(OrderConstants.order0orderItem0amount))
+			.andExpect(jsonPath("$.orderDTOList[0].orderItemDTOList[0].totalOrderedItemPrice").value(OrderConstants.order0orderItem0amount * OrderConstants.order0Price))
+			.andExpect(jsonPath("$.orderDTOList[0].orderItemDTOList[0].bookDTO.bookId").value(BookConstants.book0id))
+			.andExpect(jsonPath("$.orderDTOList[0].orderItemDTOList[0].bookDTO.name").value(BookConstants.book0name))
+			.andDo(document("{class-name}/{method-name}",preprocessResponse(prettyPrint()),
+					responseFields(bookReportDTOResponseFields())));
+				
+	}
+	
+	 private FieldDescriptor[] bookReportDTOResponseFields() {
+		    return new FieldDescriptor[] {
+		    		fieldWithPath("orderDTOList.[].orderId").description("The unique identifier of the order"),
+		    		fieldWithPath("orderDTOList.[].orderDate").description("Date when the order has been performed"),
+		    		fieldWithPath("orderDTOList.[].orderPrice").description("Date when the order has been performed"),
+		    		fieldWithPath("orderDTOList.[].orderItemDTOList.[].orderItemId").description("The unique identifier of the order item"),
+		    		fieldWithPath("orderDTOList.[].orderItemDTOList.[].orderId").description("The unique identifier of the order"),
+		    		fieldWithPath("orderDTOList.[].orderItemDTOList.[].orderedAmount").description("Amount of ordered item"),
+		    		fieldWithPath("orderDTOList.[].orderItemDTOList.[].totalOrderedItemPrice").description("Total price of ordered items/books"),
+		    		fieldWithPath("orderDTOList.[].orderItemDTOList.[].bookDTO.bookId").description("The unique identifier of the order item/book"),
+		    		fieldWithPath("orderDTOList.[].orderItemDTOList.[].bookDTO.name").description("Name of the order item/book")		    		
+		    };
+		  }
 
+	// TODO: lookup how to test InputStreamResource type
+	public void getProcessedOrdersReportTest() throws Exception {
+		
+	}
+	
 }
